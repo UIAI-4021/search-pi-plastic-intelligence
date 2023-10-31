f9e875b189d7b944a863b19f8586b8a313a3b417
Mohammad Hassan <heidary0081@gmail.com>
2023-10-31 18:18:07 +0330

    editing final part

--- Q1_Flight/data_cleaner.py
+++ Q1_Flight/data_cleaner.py
@@ -4,6 +4,9 @@ original_data = pd.read_csv('Flight_Data.csv')
 data = original_data[['SourceAirport', 'DestinationAirport', 'Distance', 'FlyTime', 'Price']]
 print('Real data examples count : ' , len(original_data))
 unrepeated_data = data.drop_duplicates(['SourceAirport', 'DestinationAirport'])
+
+
 print('Unrepeated data count', len(unrepeated_data))
 
 unrepeated_data.to_csv('train_data.csv')
+print('Done')
