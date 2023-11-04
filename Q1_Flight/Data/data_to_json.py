3f3c756b7182bb1155e02d0f6c9677436eafb4c6
Mohammad Hassan <heidary0081@gmail.com>
2023-11-05 00:05:58 +0330

    jasoner file edited
    now the graph looks like this :
    {
    city : { n1 : d1, n2 : d2 , ... }
    ...
     }

--- Q1_Flight/Data/data_to_json.py
+++ Q1_Flight/Data/data_to_json.py
@@ -1,35 +1,20 @@
 import pandas as pd
 import json
 
-data = pd.read_csv('train_data.csv')
-print('number of examples : ', len(data))
-
 graph = {}
+data = pd.read_csv('train_data.csv')
+
 for i in range(len(data)) :
-    airport = data.iloc[i].SourceAirport
-    if airport not in graph.keys() :
-        graph[airport] = {'Neighbors': [], 'Distance' : [], 'Price' : [], 'FlyTime' : []}
+    row = data.iloc[i]
+    if row.SourceAirport not in graph.keys() :
+        graph[row.SourceAirport] = {}
 
-    if airport in graph.keys() :
-        if data.iloc[i].DestinationAirport not in graph[airport]['Neighbors'] :
-            graph[airport]['Neighbors'].append(data.iloc[i].DestinationAirport)
-            graph[airport]['Distance'].append(data.iloc[i].Distance)
-            graph[airport]['FlyTime'].append(data.iloc[i].FlyTime)
-            graph[airport]['Price'].append(data.iloc[i].Price)
+    if row.SourceAirport in graph.keys() :
 
-i = 0
-for key, value in graph.items() :
-    print(key, value , end = '\n')
-    i += 1
-    if i == 5 :
-        break
-
-
-
-print(len(graph.keys()))
-print(len(graph['Imam Khomeini International Airport']['Neighbors']))
-graph = { 'Nodes' : graph}
+        if row.DestinationAirport not in graph[row.SourceAirport].keys():
+            graph[row.SourceAirport][row.DestinationAirport] = row.Distance
 
 
 with open('Nodes.json', 'w') as file :
     json.dump(graph, file)
+
