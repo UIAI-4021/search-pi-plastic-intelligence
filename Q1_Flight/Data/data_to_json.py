648bec1cf480bdc0ecb49e70343821613f1332c9
ArianJafari <arian.jafari2233@gmail.com>
2023-11-09 15:01:44 +0330

    convert csv to dictionary location

--- Q1_Flight/Data/data_to_json.py
+++ Q1_Flight/Data/data_to_json.py
@@ -3,34 +3,17 @@ import json
 
 graph = {}
 data = pd.read_csv('train_data.csv')
-print(data[data['DestinationAirport'] == 'Abraham Lincoln Capital Airport'])
 
 for i in range(len(data)) :
     row = data.iloc[i]
-    source = row.SourceAirport
-    target = row.DestinationAirport
-    distance = row.Distance
+    if row.SourceAirport not in graph.keys() :
+        graph[row.SourceAirport] = {}
 
-    if source not in graph.keys() :
-        graph[source] = {}
+    if row.SourceAirport in graph.keys() :
 
-    if source in graph.keys() :
+        if row.DestinationAirport not in graph[row.SourceAirport].keys():
+            graph[row.SourceAirport][row.DestinationAirport] = row.Distance
 
-        if target not in graph[source].keys():
-            graph[source][target] = distance
-
-    if target not in graph.keys():
-        graph[target] = {}
-
-    if target in graph.keys():
-
-        if source not in graph[target].keys():
-            graph[target][source] = distance
-
-
-
-
-print(graph['Abraham Lincoln Capital Airport'])
 
 with open('Nodes.json', 'w') as file :
     json.dump(graph, file)
